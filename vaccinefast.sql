a6dbd9d6ba77f813f794816a1c69ccda6989a0bc
matheusoliveira67 <84286981+matheusoliveira67@users.noreply.github.com>
2021-05-29 18:07:47 -0300

    Update vaccinefast.sql

--- vaccinefast.sql
+++ vaccinefast.sql
@@ -22,5 +22,20 @@ create table tb_fila_de_vacinacao (
     datavacinacao varchar(200)
 
 );
+insert into tb_atendente (nome,senha) values ('atd', '1234');
+insert into tb_administrador (nome,senha) values ('adm', '1234');
 
 select * from tb_fila_de_vacinacao;
+select * from tb_atendente;
+select * from tb_administrador;
+describe tb_administrador;
+
+
+SET GLOBAL TIME_ZONE = '-3:00';
+
+alter table tb_administrador add column cargo varchar (20) not null;
+update tb_administrador set cargo = 'Administrador' where id = 1;
+
+alter table tb_atendente add column cargo varchar (20) not null;
+update tb_atendente set cargo = 'Atendente' where id = 1;
+
